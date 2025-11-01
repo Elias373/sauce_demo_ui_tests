e3865bf89207b2568b0d49fa7f724955dbf9faec
Illia Karcheuski <korchevskii.eli@gmail.com>
2025-11-01 12:35:13 +0300

    ru -> eng

--- tests/test_simple_po.py
+++ tests/test_simple_po.py
@@ -9,12 +9,12 @@ cart = CartPage()
 
 
 
-@allure.title("1. Успешный логин")
+@allure.title("1. Success login")
 def test_successful_login():
     login.open().login('standard_user', 'secret_sauce')
     main.should_be_loaded()
 
-@allure.title("2. Добавление товара в корзину и проверка")
+@allure.title("2. Adding an item to the shopping cart and checking")
 def test_add_item_to_cart():
     login.open().login('standard_user', 'secret_sauce')
     main.add_item_to_cart('add-to-cart-sauce-labs-backpack')
@@ -22,19 +22,19 @@ def test_add_item_to_cart():
     main.go_to_cart()
     cart.should_contain_item('Sauce Labs Backpack')
 
-@allure.title("3. Открытие страницы About")
+@allure.title("3. Open the About page")
 def test_open_about_page():
     login.open().login('standard_user', 'secret_sauce')
     main.open_about_page()
     main.should_be_on_about_page()
 
-@allure.title("4. Успешный логаут")
+@allure.title("4. Success logout")
 def test_successful_logout():
     login.open().login('standard_user', 'secret_sauce')
     main.logout()
     login.should_be_loaded()
 
-@allure.title("5. Неуспешный логин")
+@allure.title("5. Invalid login (negative)")
 def test_failed_login():
     login.open().login('invalid_user', 'invalid_password')
     login.should_have_error('Username and password do not match')
